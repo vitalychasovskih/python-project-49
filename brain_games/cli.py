d3488c89d05a6c308263aace862bef51edcba71b
Vitaly Chasovskikh <>
2024-03-14 23:44:17 +0700

    3 step

---
+++ brain_games/cli.py
@@ -0,0 +1,6 @@
+import prompt 
+
+
+def welcom_user():
+	name = prompt.string('May I have your name? ')
+	print(f'Hello, {name}!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcom_user
 
 def main():
-    print("Welcome to the Brain Games!")
-
+	print("Welcome to the Brain Games!")
+	welcom_user()
 
 if __name__ == '__main__':
-    main()
+	main()
