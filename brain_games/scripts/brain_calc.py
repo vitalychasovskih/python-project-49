33c01ab3079849fe09f26c7559d273c21a964f5a
Vitaly Chasovskikh <>
2024-04-09 13:28:50 +0700

    big fix scripts/brain_calc.py after review

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.calc import calculation
+from brain_games.engine import brain_engine
+import brain_games.games.calc
 
 
 def main():
-    calculation()
+    brain_engine(brain_games.games.calc)
 
 
 if __name__ == '__main__':
