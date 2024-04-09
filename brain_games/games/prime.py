9b3ce7d8a4250348dc11d0a43dcd14c22f409367
Vitaly Chasovskikh <>
2024-04-09 13:07:24 +0700

    big fix of games/prime.py after review

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,35 +1,26 @@
-from brain_games.cli import welcom_user
-from brain_games.engine import show_description, show_question
-from brain_games.engine import cheking_the_answer
-from brain_games.engine import br_prime
-from brain_games.constants import NUMBER_OF_ROUNDS
+import random
 
 
-print('Welcome to the Brain Games!')
-name = welcom_user()
+def description():
+    prime_description = ('Answer "yes" if given number is prime. '
+                         'Otherwise answer "no".')
+    return prime_description
 
 
-def prime():
-    description, question, perfect_answer = br_prime()
-    show_description(description)
-    counter = 0
-    flag = True
-    while counter < NUMBER_OF_ROUNDS:
-        description, question, perfect_answer = br_prime()
-        show_question(question)
-        answer = input('Your answer: ')
-        chek = cheking_the_answer(answer, perfect_answer)
-        if chek:
-            print("Correct!")
-            counter += 1
-        else:
-            flag = False
-            lost_message = (
-                f"'{answer}' is wrong answer ;(. "
-                f"Correct answer was '{perfect_answer}'."
-            )
-            print(lost_message)
-            print(f"Let's try again, {name}!")
-            break
-    if flag:
-        print(f"Congratulations, {name}!")
+def is_prime(n):
+    if n % 2 == 0:
+        return n == 2
+    d = 3
+    while d * d <= n and n % d != 0:
+        d += 2
+    return d * d > n
+
+
+def logic():
+    number = random.randrange(100)
+    if is_prime(number):
+        prime_perfect_answer = 'yes'
+    else:
+        prime_perfect_answer = 'no'
+    prime_question = f'Question: {number}'
+    return prime_question, prime_perfect_answer
