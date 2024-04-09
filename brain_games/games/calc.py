825ebebdaf564462a5d07ed433b3fcee857b08a1
Vitaly Chasovskikh <>
2024-04-09 13:05:03 +0700

    big fix of games/calc.py after review

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,35 +1,22 @@
-from brain_games.cli import welcom_user
-from brain_games.engine import show_description, show_question
-from brain_games.engine import cheking_the_answer
-from brain_games.engine import br_calc
-from brain_games.constants import NUMBER_OF_ROUNDS
+import random
 
 
-print('Welcome to the Brain Games!')
-name = welcom_user()
+def description():
+    calc_description = 'What is the result of the expression?'
+    return calc_description
 
 
-def calculation():
-    description, question, perfect_answer = br_calc()
-    show_description(description)
-    counter = 0
-    flag = True
-    while counter < NUMBER_OF_ROUNDS:
-        description, question, perfect_answer = br_calc()
-        show_question(question)
-        answer = int(input('Your answer: '))
-        chek = cheking_the_answer(answer, perfect_answer)
-        if chek:
-            print("Correct!")
-            counter += 1
-        else:
-            flag = False
-            lost_message = (
-                f"'{answer}' is wrong answer ;(. "
-                f"Correct answer was '{perfect_answer}'."
-            )
-            print(lost_message)
-            print(f"Let's try again, {name}!")
-            break
-    if flag:
-        print(f"Congratulations, {name}!")
+def logic():
+    number_1 = random.randrange(100)
+    number_2 = random.randrange(100)
+    operator = random.randrange(1, 4)
+    if operator == 1:
+        calc_perfect_answer = number_1 + number_2
+        calc_question = f'Question: {number_1} + {number_2}'
+    elif operator == 2:
+        calc_perfect_answer = number_1 - number_2
+        calc_question = f'Question: {number_1} - {number_2}'
+    else:
+        calc_perfect_answer = number_1 * number_2
+        calc_question = f'Question: {number_1} * {number_2}'
+    return calc_question, calc_perfect_answer
