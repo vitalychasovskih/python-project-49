e4a9baf9fca6261c43634272c63e0d41b69a204a
Vitaly Chasovskikh <>
2024-04-09 13:30:22 +0700

    big fix scripts/brain_gcd.py after review

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games.gcd import gcd
+from brain_games.engine import brain_engine
+import brain_games.games.gcd
 
 
 def main():
-    gcd()
+    brain_engine(brain_games.games.gcd)
 
 
 if __name__ == '__main__':
