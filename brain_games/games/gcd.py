9f61ff22923643803f4a391fc0f07696f0eee318
Vitaly Chasovskikh <>
2024-04-11 22:13:05 +0700

    fix gcd.py after 2 review

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,17 +1,12 @@
-import random
-
+from random import randrange
+from math import gcd
 
 description = 'Find the greatest common divisor of given numbers.'
 
 
 def logic():
-    number_1 = random.randrange(100)
-    number_2 = random.randrange(100)
+    number_1 = randrange(100)
+    number_2 = randrange(100)
     question = f'{number_1} {number_2}'
-    while number_1 != 0 and number_2 != 0:
-        if number_1 > number_2:
-            number_1 = number_1 % number_2
-        else:
-            number_2 = number_2 % number_1
-    gcd_perfect_answer = number_1 + number_2
+    gcd_perfect_answer = gcd(number_1, number_2)
     return question, str(gcd_perfect_answer)
