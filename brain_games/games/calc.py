808491869b5ae13ec3c115a1867e4166431d190a
Vitaly Chasovskikh <>
2024-04-14 18:07:04 +0700

    refactor calc.py: add random.choise()

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,4 +1,4 @@
-from random import randrange
+from random import randrange, choice
 
 description = 'What is the result of the expression?'
 
@@ -10,14 +10,17 @@ def logic():
     """
     number_1 = randrange(100)
     number_2 = randrange(100)
-    operator = randrange(1, 4)
-    if operator == 1:
-        calc_perfect_answer = number_1 + number_2
-        question = f'{number_1} + {number_2}'
-    elif operator == 2:
-        calc_perfect_answer = number_1 - number_2
-        question = f'{number_1} - {number_2}'
-    else:
-        calc_perfect_answer = number_1 * number_2
-        question = f'{number_1} * {number_2}'
+    operator = choice(['+', '-', '*'])
+    question = f'{number_1} {operator} {number_2}'
+    calc_perfect_answer = eval(question)
+    # operator = randrange(1, 4)
+    # if operator == 1:
+    #     calc_perfect_answer = number_1 + number_2
+    #     question = f'{number_1} + {number_2}'
+    # elif operator == 2:
+    #     calc_perfect_answer = number_1 - number_2
+    #     question = f'{number_1} - {number_2}'
+    # else:
+    #     calc_perfect_answer = number_1 * number_2
+    #     question = f'{number_1} * {number_2}'
     return question, str(calc_perfect_answer)
