85d552b7224242f6af82218a77b2163876d27476
Vitaly Chasovskikh <>
2024-04-14 18:14:20 +0700

    style: in calc.py removed commented lines after successful hexlet-check

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -13,14 +13,4 @@ def logic():
     operator = choice(['+', '-', '*'])
     question = f'{number_1} {operator} {number_2}'
     calc_perfect_answer = eval(question)
-    # operator = randrange(1, 4)
-    # if operator == 1:
-    #     calc_perfect_answer = number_1 + number_2
-    #     question = f'{number_1} + {number_2}'
-    # elif operator == 2:
-    #     calc_perfect_answer = number_1 - number_2
-    #     question = f'{number_1} - {number_2}'
-    # else:
-    #     calc_perfect_answer = number_1 * number_2
-    #     question = f'{number_1} * {number_2}'
     return question, str(calc_perfect_answer)
