e996224315cb7f3f6a882d07b96b5e34449484ab
Vitaly Chasovskikh <>
2024-04-14 14:58:01 +0700

    docs: add docstrings in even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,6 +4,9 @@ description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def logic():
+    """
+    Create a number, create and return a question and the expected answer.
+    """
     number = randrange(100)
     even_perfect_answer = 'yes' if number % 2 == 0 else 'no'
     question = f'{number}'
