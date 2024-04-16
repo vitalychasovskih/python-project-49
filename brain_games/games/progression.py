7b7ee1f078321bf3408d9dc5c7052938eff8afcc
Vitaly Chasovskikh <>
2024-04-17 01:46:10 +0700

    refac: in progression.py function progression now will receive parameters

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,14 +3,15 @@ from random import randint, randrange
 description = 'What number is missing in the progression?'
 
 
-def make_progression():
+def make_progression(length, first_element, step):
     """
-    Return a progression of random length
-    with a random first element and a random step.
+    Return a progression with the given parameters.
+
+    Args:
+    length: int
+    first_element: int
+    step: int
     """
-    length = randint(5, 10)
-    first_element = randint(1, 20)
-    step = randint(2, 7)
     progression = []
     for i in range(length):
         progression.append(first_element + i * step)
@@ -38,7 +39,10 @@ def logic():
     Create a progression, select a random element, create and return a question
     and the expected answer.
     """
-    progression = make_progression()
+    length = randint(5, 10)
+    first_element = randint(1, 20)
+    step = randint(2, 7)
+    progression = make_progression(length, first_element, step)
     index_question = randrange(len(progression))
     question = make_question(progression, index_question)
     pr_perfect_answer = str(progression[index_question])
