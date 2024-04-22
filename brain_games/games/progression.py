52ee8a37f6d233f3b186989cd7117a59f305924a
Vitaly Chasovskikh <>
2024-04-23 00:07:52 +0700

    refac: rename progression terms in progression.py after 4 review

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,24 +3,24 @@ from random import randint, randrange
 DESCRIPTION = 'What number is missing in the progression?'
 NIN_LENGTH = 5
 MAX_LENGTH = 10
-MIN_FIRST = 1
-MAX_FIRST = 20
-MIN_STEP = 2
-MAX_STEP = 7
+MIN_INITIAL = 1
+MAX_INITIAL = 20
+MIN_DIFFERENCE = 2
+MAX_DIFFERENCE = 7
 
 
-def make_progression(length, first_element, step):
+def make_progression(length, initial, difference):
     """
     Return a progression with the given parameters.
 
     Args:
     length: int
-    first_element: int
-    step: int
+    initial: int
+    difference: int
     """
     progression = []
     for i in range(length):
-        progression.append(first_element + i * step)
+        progression.append(initial + i * difference)
     return progression
 
 
@@ -45,12 +45,12 @@ def make_question_and_perfect_answer():
     Create a progression, select a random element, create and return a question
     and the expected answer.
     """
-    length_progression = randint(NIN_LENGTH, MAX_LENGTH)
-    first_element_progression = randint(MIN_FIRST, MAX_FIRST)
-    step_progression = randint(MIN_STEP, MAX_STEP)
-    progression = make_progression(length_progression,
-                                   first_element_progression,
-                                   step_progression
+    length_of_progression = randint(NIN_LENGTH, MAX_LENGTH)
+    initial_of_progression = randint(MIN_INITIAL, MAX_INITIAL)
+    difference_of_progression = randint(MIN_DIFFERENCE, MAX_DIFFERENCE)
+    progression = make_progression(length_of_progression,
+                                   initial_of_progression,
+                                   difference_of_progression
                                    )
     index_question = randrange(len(progression))
     question = construct_question(progression, index_question)
